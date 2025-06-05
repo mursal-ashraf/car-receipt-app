18f277e5fabda73cc7cfb9031ca748a8eb797957
Mursal Ashraf <mash0019@student.monash.edu>
2025-06-05 22:36:32 +1000

    add modifications for different basename

--- src/Router/AppRouter.tsx
+++ src/Router/AppRouter.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { AppRoutes } from './routes';
 
-export const router = createBrowserRouter(AppRoutes);
+export const router = createBrowserRouter(AppRoutes, {
+  basename: '/car-receipt-app',
+});
 
 export const AppRouter: React.FC = () => <RouterProvider {...{ router }} />;
